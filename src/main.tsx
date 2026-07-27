eef55e802de791874edd1e070d1fd6c36ecc4b5c
samayms <samayms@umich.edu>
2026-07-27 15:34:15 -0400

    Add interactive font review lab

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import {
+  applyFontVariant,
+  FontReviewControls,
+  fontVariantFromSearch,
+} from "./ui/FontReview.tsx";
 import "./index.css";
+
+applyFontVariant(fontVariantFromSearch(window.location.search));
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <FontReviewControls />
   </StrictMode>,
 );
